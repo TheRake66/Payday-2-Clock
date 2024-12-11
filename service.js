a590351bbe4e0d2c2b513889bdfb5b1064e998a4
TheRake66 <65202670+TheRake66@users.noreply.github.com>
2024-12-11 20:56:03 +0100

    support shitty firefox browser

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 /**
  * @type {String} L'identifiant du fichier de cache.
  */
-const cacheName = 'pwa-cache-v9';
+const cacheName = 'pwa-cache-v13';
 
 /**
  * @type {Array} Liste des fichiers à mettre en cache.
